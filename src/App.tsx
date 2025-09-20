e4148a71847d1bb414c44518d11e8b3a5a7abf13
AnkitPatel1999 <ankitpatelas90@gmail.com>
2025-09-20 13:30:06 +0530

    doughnutchart and common css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/common/Header'
+import Hero from './components/hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
+      <div className="ae-max-width-1440">
+        <Header />
+        <div className='ae-mx-28 ae-my-28'>
+          <Hero />
+        </div>
+      </div>
     </>
   )
 }
